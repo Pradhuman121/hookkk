183481bb6febf82a6fcc496c92c0eff8f5e539f8
Pradhuman121 <pradhuman.singh@involead.com>
2024-03-29 09:58:01 +0530

    checkkk

--- python.py
+++ python.py
@@ -9,10 +9,23 @@
 
 
 # Example of Python code with linting issues
-def calculate_square(num):
-     if num < 0:
-        print("Number must be non-negative")
-     return num**2
+# def calculate_square(num):
+#     if num < 0:
+#         print("Number must be non-negative"):
+#     return num**2
 
 
-result = calculate_square(5)
+# result = calculate_square(5)
+
+
+# Example of Python code with linting issues
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib
+
+print(f"The first 10 Fibonacci numbers are: {fibonacci(10)}")
+
+def sum(a, b):
+    return a+b
